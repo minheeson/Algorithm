b91170043b113590e211b5256c6c7c094fa16946
sonminhee <lois7109@gmail.com>
2018-01-11 11:07:19 +0900

    add 10971

--- BOJ/Baek_10971.java
+++ BOJ/Baek_10971.java
@@ -1,68 +1,65 @@
 package baek_10971;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Baek_10971 {
-	static int[][] weight;
-	static int[][] visited;
+
+	static boolean[] visited;
+	static int n; // 도시의 수
+	static int[][] cost;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		weight = new int[size][size];
-		visited = new int[size][size];
-		int[] answer = new int[size];
-		int minAns = 0;
+		n = sc.nextInt();
 
-		for (int i = 0; i < size; i++) {
-			answer[i] = 0;
-			for (int j = 0; j < size; j++) {
-				weight[i][j] = sc.nextInt();
-				visited[i][j] = 0; // 미방문으로 초기화
+		cost = new int[n][n];
+		visited = new boolean[n];
+		ArrayList<Integer> path = new ArrayList<>();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				cost[i][j] = sc.nextInt();
 			}
+			visited[i] = false;
 		}
 
-		int valueJ = Integer.MAX_VALUE;
+		path.add(0);
+		System.out.println(shortestPath(path, 0));
 
-		for (int i = 0; i < size; i++) { // i는 처음으로 방문할 지역
+	}
 
-			int min = Integer.MAX_VALUE;
+	static int shortestPath(ArrayList<Integer> path, int curLength) {
 
-			for (int j = 0; j < size; j++) {
-				System.out.println("TEST J 1:: ");
-				visited[j][i] = 1;
-			}
-
-			for (int j = 0; j < size; j++) {
-				if (weight[i][j] != 0 && visited[i][j] == 0) {
-					if (min > weight[i][j]) {
-						min = weight[i][j];
-						valueJ = j;
-						System.out.println("TEST J 2:: " + min);
-					}
-				}
-				answer[i] = answer[i] + min;
-				visited[i][valueJ] = 1;
-				visited[valueJ][i] = 1;
-
-			}
-			System.out.println("TEST J :: " + valueJ);
-
+		if (path.size() == n) {
+			return curLength + cost[path.get(path.size() - 1)][0];
 		}
 
-		for (int i = 0; i < size - 1; i++) {
-			minAns = answer[i];
-			System.out.println(minAns);
-			if (minAns > answer[i + 1]) {
-				minAns = answer[i + 1];
-			}
+		int ret = Integer.MAX_VALUE;
 
+		for (int next = 1; next < n; next++) {
+
+			if (visited[next])
+				continue;
+			System.out.println("next :: " + next);
+
+			int here = path.get(path.size() - 1);
+			System.out.println("here :: " + here);
+			if (cost[here][next] == 0)
+				continue;
+			path.add(next);
+			visited[next] = true;
+			int cand = shortestPath(path, curLength + cost[here][next]);
+			System.out.println("<cand> :: " + cand);
+			ret = Math.min(ret, cand);
+			visited[next] = false;
+			System.out.println("----------------  " + next + " 지워짐  ");
+			path.remove(path.size() - 1);
 		}
 
-		System.out.println(minAns);
-
+		return ret;
 	}
 
 }
