5b4c010564b5d325cdf2cbd737a22fef6a007651
sonminhee <lois7109@gmail.com>
2018-02-11 22:48:09 +0900

    chap01 problems

--- Interview/chap_01/p01_04.java
+++ Interview/chap_01/p01_04.java
@@ -5,31 +5,74 @@ public class p01_04 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String test = "aabcccdeff";
+		String test = "abcd";
+		String test2 = "aaaaaabccdee";
 		System.out.println(compress(test));
+		System.out.println(betterCompress(test));
+
+		System.out.println(compress(test2));
+		System.out.println(betterCompress(test2));
 	}
 
 	static String compress(String s) {
 		int count = 1;
 		String rtn = "";
+		char last = s.charAt(0);
 		for (int i = 1, len = s.length(); i < len; i++) {
-
-			if (s.charAt(i - 1) != s.charAt(i)) {
-				rtn += s.charAt(i - 1);
-				rtn += count;
+			if (last != s.charAt(i)) {
+				rtn = rtn + last + count;
 				count = 1;
-
+				last = s.charAt(i);
 			} else {
 				count++;
-			}
-			if (i == s.length() - 1) {
-				rtn += s.charAt(i - 1);
-				rtn += count;
-				return rtn;
 			}
 
 		}
 
-		return rtn;
+		return rtn + last + count;
+	}
+
+	static int countCompress(String s) {
+		int size = 0;
+		int count = 1;
+		char last = s.charAt(0);
+
+		for (int i = 1, len = s.length(); i < len; i++) {
+			if (last != s.charAt(i)) {
+				size = size + 1 + String.valueOf(count).length();
+				count = 1;
+				last = s.charAt(i);
+			} else {
+				count++;
+			}
+		}
+
+		return size + 1 + String.valueOf(count).length();
+	}
+
+	static String betterCompress(String s) {
+		if (s.length() < countCompress(s)) {
+			return s;
+		}
+
+		StringBuffer sb = new StringBuffer();
+		char last = s.charAt(0);
+		int count = 1;
+
+		for (int i = 1, len = s.length(); i < len; i++) {
+			if (last != s.charAt(i)) {
+				sb.append(last);
+				sb.append(count);
+				last = s.charAt(i);
+				count = 1;
+			} else {
+				count++;
+			}
+		}
+
+		sb.append(last);
+		sb.append(count);
+		
+		return sb.toString();
 	}
 }
